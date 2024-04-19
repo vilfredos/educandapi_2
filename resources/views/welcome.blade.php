f01990c87fb8c1af8fe18feb52a7689d55ce05c6
vilfredos <103297447+vilfredos@users.noreply.github.com>
2024-04-19 16:58:22 -0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UMSS</title>
+    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+</head>
+
+<body>
+<p>hello world</p>
+</body>
+</html>
